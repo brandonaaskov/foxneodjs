ddec42d939b15c82395242716e3da920ce978e4a
Brandon Aaskov <aaskov@gmail.com>
2013-04-25 18:32:11 -0700

    In the middle of a refactoring effort, but heading home for the night so I'll pick this up on my home machine.

--- web/FoxNEO.temp.js
+++ web/FoxNEO.temp.js
@@ -96,135 +96,163 @@
 
         //-------------------------------------------------------------------------------- public methods
 
-        //--------------------------------------- core features
-        var setPlayerMessage = function (options) {
-            displayModal(options);
-        };
-        //--------------------------------------- /core features
-
-
-
-
-        //--------------------------------------- iframe
-        var iframe = function () {
-            var playerIds = []; // stores the ids of the elements we find
-
-            (function iframeInit () {
-//                console.log('iframe init');
-            })();
-
-            var getPlayers = function (selector) {
-                var players = [];
-
-                if (selector)
-                {
-                    players = document.querySelectorAll(selector);
-                }
-
-                return players;
-            };
-
-            var getPlayerAttributes = function (element, attributePrefix) {
-                var allAttributes = element.attributes;
-                var playerAttributes = {};
-
-                var addPlayerAttribute = function (key, value) {
-                    playerAttributes[key] = value;
+        //--------------------------------------- player
+        var player = function () {
+            var core = function () {
+                var setPlayerMessage = function (options) {
+                    displayModal(options);
                 };
 
-                for (var i = 0, n = allAttributes.length; i < n; i++)
-                {
-                    var attr = allAttributes[i],
-                        attributePrefix = 'data-' || attributePrefix,
-                        attrName = attr.nodeName;
+                return {
+                    setPlayerMessage: setPlayerMessage
+                }
+            };
 
-                    if (attrName === 'id')
+            //--------------------------------------- iframe
+            var iframe = function () {
+                var playerIds = []; // stores the ids of the elements we find
+
+                (function iframeInit () {
+//                console.log('iframe init');
+                })();
+
+                var getPlayers = function (selector) {
+                    var players = [];
+
+                    if (selector)
                     {
-                        playerIds.push(attr.nodeValue);
-                        addPlayerAttribute(attrName, attr.nodeValue);
+                        players = document.querySelectorAll(selector);
                     }
 
-                    if (attrName.indexOf('data-') !== -1)
-                    {
-                        var key = attrName.substr(attributePrefix.length);
-                        var value = '';
+                    return players;
+                };
 
-                        /**
-                         * Some of these cases are just for defaults, but some are for fixing casing. These keys
-                         * eventually get passed to the player object on the page, and some browser (and jQuery)
-                         * lowercase all attribute names, which means we have to switch them back to the proper case
-                         * otherwise they won't work in the player properly.
-                         */
-                        switch (key.toLowerCase())
+                var getPlayerAttributes = function (element) {
+                    var allAttributes = element.attributes;
+                    var playerAttributes = {};
+
+                    for (var i = 0, n = allAttributes.length; i < n; i++)
+                    {
+                        var attr = allAttributes[i],
+                            attrName = attr.nodeName;
+
+                        if (attrName.indexOf('data-player') !== -1)
                         {
-                            case 'width':
-                                value = attr.nodeValue || 640;
-                                break;
-                            case 'height':
-                                value = attr.nodeValue || 360;
-                                break;
-                            case 'releaseurl':
-                                key = 'releaseURL';
-                                value = attr.nodeValue;
-                                break;
-                            case 'sitesection':
-                                key = 'siteSection';
-                                break;
-                            default:
-                                value = attr.nodeValue;
-                                break;
-                        }
+                            var attributes = attr.nodeValue.split('|');
 
-                        addPlayerAttribute(key, value);
+                            for (var j = 0, m = attributes.length; j < m; j++)
+                            {
+                                var keyValuePair = attributes[j].split('=');
+                                playerAttributes[keyValuePair[0]] = keyValuePair[1];
+                            }
+                        }
+                    }
+
+                    return playerAttributes;
+                };
+
+
+                var injectIframe = function (attributes, iframeURL) {
+                    var elements = document.querySelectorAll('#' + attributes.id);
+
+                    if (elements.length === 1)
+                    {
+                        var attributesString = utils().objectToPipeString(attributes);
+                        console.log('attributesString', attributesString);
+                        var domElement = elements[0];
+                        domElement.innerHTML = '<iframe ' +
+                            'src="'+ iframeURL + '?' +
+                            'playerParams=' + attributesString + '"' +
+                            'scrolling="no" ' +
+                            'frameborder="0" ' +
+                            'width="' + attributes.width + '"' +
+                            'height="'+ attributes.height + '"></iframe>';
+                    }
+                    else if (elements.length > 1)
+                    {
+                        throw new Error('The HTML ID you used for the player must be unique, ' +
+                            'but there is more than one in this page.');
+                    }
+                };
+
+                var swapPlayers = function (selector, iframeURL) {
+                    var players = getPlayers(selector);
+
+                    for (var i = 0, n = players.length; i < n; i++)
+                    {
+                        var player = players[i];
+                        var attributes = getPlayerAttributes(player);
+
+                        injectIframe(attributes, iframeURL);
+                    }
+                };
+
+                // Public API
+                return {
+                    swapPlayers: swapPlayers
+                };
+            };
+            //--------------------------------------- /iframe
+
+            return {
+                setPlayerMessage: core.setPlayerMessage,
+                iframe: iframe
+            }
+        };
+        //--------------------------------------- /player
+
+
+
+        //--------------------------------------- utils
+        var utils = function () {
+            var arrayToObject = function (arr) {
+                var obj = {};
+
+                for (var i = 0, n = arr.length; i < n; i++)
+                {
+                    var item = arr[i];
+                    if (item.indexOf('=') !== -1)
+                    {
+                        var itemPieces = item.split('=');
+
+                        obj[itemPieces[0]] = itemPieces[1];
                     }
                 }
 
-                return playerAttributes;
+                return obj;
             };
 
+            var objectToPipeString = function (obj) {
+                var properties = [];
 
-            var injectIframe = function (attributes) {
-                console.log('attributes', attributes);
-
-                var elements = document.querySelectorAll('#' + attributes.id);
-
-                if (elements.length === 1)
+                for (var prop in obj)
                 {
-                    var domElement = elements[0];
-                    //TODO: change this url reference
-                    domElement.innerHTML = '<iframe ' +
-                        'src="http://baaskov.local/tests/fox/btn/iframe-player.html?' +
-                        'playerParams=' + btoa(JSON.stringify(attributes)) + '"' +
-                        'scrolling="no" ' +
-                        'frameborder="0" ' +
-                        'width="' + attributes.width + '"' +
-                        'height="'+ attributes.height + '"></iframe>';
+                    properties.push(prop + '=' + obj[prop]);
                 }
-                else if (elements.length > 1)
-                {
-                    throw new Error('The HTML ID you used for the player must be unique, ' +
-                        'but there is more than one in this page.');
-                }
+
+                return properties.join('|');
             };
 
-            var swapPlayers = function (selector) {
-                var players = getPlayers(selector);
+            var getRandomColor = function () {
+                var letters = '0123456789ABCDEF'.split('');
+                var color = '#';
 
-                for (var i = 0, n = players.length; i < n; i++)
+                for (var i = 0; i < 6; i++)
                 {
-                    var player = players[i];
-                    var attributes = getPlayerAttributes(player);
-
-                    injectIframe(attributes);
+                    color += letters[Math.round(Math.random() * 15)];
                 }
-            };
+
+                return color;
+            }
 
             // Public API
             return {
-                swapPlayers: swapPlayers
-            };
+                arrayToObject: arrayToObject,
+                objectToPipeString: objectToPipeString,
+                getRandomColor: getRandomColor
+            }
         };
-        //--------------------------------------- /iframe
+        //--------------------------------------- /utils
 
 
 
@@ -233,12 +261,20 @@
             var urlString = url || window.location.href;
 
             var getQueryParams = function (returnArray) {
-                var queryParams = {};
+                var whatToReturn = {};
 
                 if (urlString.indexOf('?') !== -1)
                 {
                     var urlSplit = urlString.split('?');
-                    queryParams = urlSplit[1].split('&');
+                    var query{ara,}
+                    if (urlSplit[1].indexOf('|') !== -1)
+                    {
+                        queryParams = urlSplit[1]
+                    }
+                    else
+                    {
+                        queryParams = urlSplit[1].split('&');
+                    }
                 }
 
                 if (returnArray)
@@ -246,7 +282,7 @@
                     queryParams = objectToArray(queryParams);
                 }
 
-                return queryParams;
+                return whatToReturn;
             };
 
             var paramExists = function (key, value) {
@@ -274,14 +310,41 @@
             };
 
             var getParamValue = function (key) {
-
                 if (paramExists(key))
                 {
                     var queryParams = getQueryParams();
 
                     for (var i = 0, n = queryParams.length; i < n; i++)
                     {
-                        var keyValuePair = queryParams[i].split('=');
+                        var queryParam = queryParams[i],
+                            keyValuePair = [];
+
+                        if (queryParam.indexOf('|') !== -1) //we found a pipe string, so we handle it differently
+                        {
+                            var pipeStringPieces = queryParam.split('|');
+                            console.log('queryParam', queryParam);
+                            console.log('pipestringpieces', pipeStringPieces);
+
+                            for (var j = 0, m = pipeStringPieces.length; j < m; j++)
+                            {
+                                var pipeStringPiece = pipeStringPieces[j];
+                                console.log('---------', pipeStringPiece);
+                                keyValuePair = pipeStringPiece.split('=');
+
+                                if (keyValuePair[0] === key)
+                                {
+                                    console.log('!!!!!!!!', keyValuePair);
+                                    return keyValuePair[1];
+                                }
+                            }
+                        }
+                        else
+                        {
+                            keyValuePair = queryParams[i].split('=');
+                        }
+
+
+                        console.log('!!!!!!!!', keyValuePair);
 
                         if (keyValuePair[0] === key)
                         {
@@ -301,6 +364,7 @@
             };
         };
         //--------------------------------------- /url
+
 
         //-------------------------------------------------------------------------------- /public methods
 
@@ -362,51 +426,13 @@
 
             return outputArray;
         };
-
-        var arrayToObject = function (arr) {
-            var obj = {};
-
-            for (var i = 0, n = arr.length; i < n; i++)
-            {
-                var item = arr[i];
-                if (item.indexOf('=') !== -1)
-                {
-                    var itemPieces = item.split('=');
-
-                    obj[itemPieces[0]] = itemPieces[1];
-                }
-            }
-
-            return obj;
-        };
-
-        var getRandomColor = function () {
-            var letters = '0123456789ABCDEF'.split('');
-            var color = '#';
-
-            for (var i = 0; i < 6; i++)
-            {
-                color += letters[Math.round(Math.random() * 15)];
-            }
-
-            return color;
-        }
         //-------------------------------------------------------------------------------- /private methods
 
         //-------------------------------------------------------------------------------- Public API
         return {
-            player: {
-                setPlayerMessage: setPlayerMessage,
-                iframe: iframe
-            },
-            utils: {
-                url: url,
-                objectToArray: objectToArray,
-                arrayToObject: arrayToObject,
-                getRandomColor: getRandomColor,
-                btoa: window.btoa,
-                atob: window.atob
-            }
+            player: player,
+            url: url,
+            utils: utils
         };
         //-------------------------------------------------------------------------------- /Public API
     };
