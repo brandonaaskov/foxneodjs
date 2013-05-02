6267f3aed9ff1748cd650d9bd01978389c935a59
Brandon Aaskov <aaskov@gmail.com>
2013-05-01 23:20:33 -0700

    Finally got this thing setup and working in a way that allows it to be available BEFORE the onload event. Very important.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,51 +14,6 @@
                     jshintrc: '.jshintrc'
                 },
                 files: ['Gruntfile.js', 'web/js/*.js']
-//                beforeconcat: ['web/js/app/**/*.js']
-            },
-
-//            replace: {
-//                "Replacing Items in config.js": { //can be any name, might as well make it descriptive
-//                    options: {
-//                        prefix: '@@', //this is actually the default
-//                        variables: {
-//                            'projectName': '<%= pkg.name %>',
-//                            'version': '<%= pkg.version %>',
-//                            'buildDate': '<%= grunt.template.date('+timestamp+', "yyyy-mm-dd hh:mm:ss") %>', //1996-11-10
-//                            'timestamp': timestamp
-//                        },
-//                        files: [
-//                            {
-//                                src: ['web/js/config.js'],
-//                                dest: 'web/build/test/'
-//                            }
-//                        ]
-//                    }
-//                }
-//            },
-
-            replace: {
-                "Replacing Items in config.js": { //can be any name, might as well make it descriptive
-                    src: ['web/js/config.js'],             // source files array (supports minimatch)
-                    dest: 'web/build/tmp/',             // destination directory or file
-                    replacements: [
-                    {
-                        from: '@@packageName',                   // string replacement
-                        to: '<%= pkg.name %>'
-                    },
-                    {
-                        from: '@@version',                   // string replacement
-                        to: '<%= pkg.version %>'
-                    },
-                    {
-                        from: '@@buildDate',      // regex replacement ('Fooo' to 'Mooo')
-                        to: '<%= grunt.template.date('+timestamp+', "yyyy-mm-dd hh:mm:ss") %>'
-                    },
-                    {
-                        from: '@@timestamp',
-                        to: timestamp
-                    }]
-                }
             },
 
             requirejs: {
@@ -99,8 +54,9 @@
                         //making it easy to create stand-alone libraries that do not mandate
                         //the end user use requirejs.
 //                        wrap: {
-//                            start: '(function () {',
-//                            end: '}());'
+//                            start: '(function (window, undefined) {',
+//                            end: "\twindow.FoxNEO = window.$f = FoxNEO;\n" +
+//                                "}(window, undefined));"
 //                        },
 
                         //Another way to use wrap, but uses default wrapping of:
@@ -113,7 +69,7 @@
                         //system. The example below will rename define() calls to FoxNEO.define().
                         //See http://requirejs.org/docs/faq-advanced.html#rename for a more
                         //complete example.
-                        namespace: 'FoxNEO',
+//                        namespace: 'FoxNEO',
 
                         // uses prefixed commas - makes for easy commenting-out (cmd + /)
                         paths: {
@@ -132,23 +88,29 @@
                 }
             },
 
-//            "regex-replace": {
-//                afterRequire: { //specify a target with any name
-//                    src: ['web/build/<%= pkg.name %>-<%= pkg.version %>.js'],
-//                    actions: [
-//                        {
-//                            search: '@@version',
-//                            replace: '<%= pkg.version %>',
-//                            flags: 'g'
-//                        },
-//                        {
-//                            search: '@@timestamp',
-//                            replace: '<%= grunt.template.date(new Date().getTime(), "yyyy-mm-dd hh:mm:ss") %>',
-//                            flags: 'g'
-//                        }
-//                    ]
-//                }
-//            },
+            replace: {
+                "Replacing Items RequireJS Output": { //can be any name, might as well make it descriptive
+                    src: 'web/build/<%= pkg.name %>-<%= pkg.version %>.js',
+                    dest: 'web/build/<%= pkg.name %>-<%= pkg.version %>.js',
+                    replacements: [
+                        {
+                            from: '@@packageName',
+                            to: '<%= pkg.name %>'
+                        },
+                        {
+                            from: '@@version',
+                            to: '<%= pkg.version %>'
+                        },
+                        {
+                            from: '@@buildDate',
+                            to: '<%= grunt.template.date('+timestamp+', "yyyy-mm-dd hh:mm:ss") %>'
+                        },
+                        {
+                            from: '@@timestamp',
+                            to: timestamp
+                        }]
+                }
+            },
 
             uglify: {
                 options: {
@@ -167,20 +129,6 @@
                 }
             },
 
-//            concat: {
-//                options: {
-//                    separator: ';',
-//                    stripBanners: true,
-//                    process: false,
-//                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-//                        '<%= grunt.template.today("yyyy-mm-dd") %> */'
-//                },
-//                basic: {
-//                    src: ['web/js/lib/requirejs/require.js', 'web/build/<%= pkg.name %>.js'],
-//                    dest: 'web/build/<%= pkg.name %>-<%= pkg.version %>.js'
-//                }
-//            },
-
             watch: {
                 files: ['<%= jshint.files %>'],
                 tasks: ['dev']
@@ -189,15 +137,12 @@
 
         grunt.loadNpmTasks('grunt-contrib-uglify');
         grunt.loadNpmTasks('grunt-contrib-requirejs');
-//        grunt.loadNpmTasks('grunt-contrib-concat');
         grunt.loadNpmTasks('grunt-contrib-jshint');
         grunt.loadNpmTasks('grunt-contrib-watch');
-//        grunt.loadNpmTasks('grunt-replace');
         grunt.loadNpmTasks('grunt-text-replace');
-//        grunt.loadNpmTasks('grunt-regex-replace');
 
         grunt.registerTask('default', ['jshint', 'replace', 'requirejs', 'uglify']);
-        grunt.registerTask('dev', ['jshint', 'replace', 'requirejs']);
-        grunt.registerTask('prod', ['jshint', 'replace', 'requirejs', 'uglify']);
+        grunt.registerTask('dev', ['jshint', 'requirejs', 'replace']);
+        grunt.registerTask('prod', ['jshint', 'requirejs', 'replace', 'uglify']);
     };
 })();
---
+++ web/js/foxneo.js
@@ -0,0 +1,89 @@
+/*global define, FDM_Player_vars, $pdk, _ */
+
+define(['polyfills', 'player/iframe', 'config', 'debug'], function (polyfills, iframe, config, debug) {
+    'use strict';
+
+    var buildTimestamp = '@@buildDate';
+
+    //-------------------------------------------------------------------------------- public methods
+    var setPlayerMessage = function (options) {
+        if (_.isObject(options))
+        {
+            displayModal(options);
+        }
+        else
+        {
+            debug.log('setPlayerMessage expected 1 argument: an object of options.', options);
+        }
+    };
+    //-------------------------------------------------------------------------------- /public methods
+
+
+
+    //-------------------------------------------------------------------------------- private methods
+    var displayModal = function (options) {
+        var modalOptions = {
+            message: '',
+            clearAfter: 0, //time in seconds: 0 means it will stay on screen indefinitely
+            resetPlayer: false
+        };
+
+        for (var prop in options)
+        {
+            if (modalOptions.hasOwnProperty(prop))
+            {
+                modalOptions[prop] = options[prop];
+            }
+        }
+
+        try
+        {
+            if (FDM_Player_vars.isFlash && _.isObject($pdk))
+            {
+                if (modalOptions.resetPlayer)
+                {
+                    $pdk.controller.resetPlayer();
+                }
+
+                $pdk.controller.setPlayerMessage(modalOptions.message, modalOptions.clearAfter);
+            }
+            else if (FDM_Player_vars.isIOS)
+            {
+                debug.log('HTML5');
+                //handle this in HTML5 with a card
+                if (modalOptions.resetPlayer)
+                {
+                    debug.log('resetting player');
+
+                    var tpPlayers = document.querySelectorAll('.tpPlayer');
+
+                    for (var i = 0, n = tpPlayers.length; i < n; i++)
+                    {
+                        var tpPlayer = tpPlayers[i];
+
+                        debug.log(tpPlayer);
+                    }
+                }
+            }
+        }
+        catch (exception)
+        {
+            throw new Error(exception);
+            // TODO: handle this, or get to a point where i don't have to use a try/catch
+        }
+    };
+    //-------------------------------------------------------------------------------- /private methods
+
+    //-------------------------------------------------------------------------------- initialization
+    (function init () {
+        debug.log(config.packageName + ' initialized. Built (@@buildDate)');
+    })();
+    //-------------------------------------------------------------------------------- /initialization
+
+    // Public API
+    return {
+        version: config.version,
+        setPlayerMessage: setPlayerMessage,
+        iframe: iframe
+    };
+});
