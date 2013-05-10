1276e1da89f7253c27171686e8e442981df76589
Brandon Aaskov <aaskov@gmail.com>
2013-05-09 19:13:38 -0700

    So far I'm not blown away, but I've only scratched the surface. I may end up sticking with QUnit just because it's so dead simple it's a lot easier to get people on board.

--- tests/mocha/tests.js
+++ tests/mocha/tests.js
@@ -1,27 +1,97 @@
-var assert = chai.assert,
-    expect = chai.expect,
-    should = chai.should;
+/*global require */
 
-suite('FoxNEOD', function () {
-
-    setup(function () {
-        console.log('Mocha setup');
-    });
-
-    suite('utils', function () {
-        test('adfasdfasdf', function () {
-        });
-//        test('addPixelSuffix', function () {
-//            strictEqual($f.utils.addPixelSuffix('12'), '12px', 'Adds the "px" suffix to a string passed in with no existing "px" in it.');
-//            strictEqual($f.utils.addPixelSuffix(12), '12px', 'Adds the "px" suffix to a number passed in.');
-//            strictEqual($f.utils.addPixelSuffix('30px'), '30px', 'Adds the "px" suffix to a string passed in that already has a "px" suffix.');
-//        });
-//
-//        test('removePixelSuffix', function () {
-//            strictEqual($f.utils.removePixelSuffix('12'), '12', 'Removes the "px" suffix to a string passed in with no existing "px" in it.');
-//            strictEqual($f.utils.removePixelSuffix(12), '12', 'Removes the "px" suffix to a number passed in.');
-//            strictEqual($f.utils.removePixelSuffix('30px'), '30', 'Removes the "px" suffix to a string passed in that already has a "px" suffix.');
-//        });
-    });
-
+require.config({
+    baseUrl: '../../web/js/',
+    paths: {
+        almond: 'lib/almond/almond'
+        , jquery: '../../web/js/lib/jquery/jquery-2.0.0.min'
+        , underscore: 'lib/underscore/underscore'
+        , modernizr: '../../web/js/lib/modernizr/modernizr.custom'
+        , mocha: '../../node_modules/mocha/mocha'
+        , chai: '../../node_modules/chai/chai'
+        , utils: 'utils'
+    }
 });
+
+require(['require', 'chai', 'utils', 'mocha', 'underscore'], function (require, chai, utils) {
+    'use strict';
+
+    mocha.setup('tdd');
+
+    var assert = chai.assert,
+        expect = chai.expect,
+        should = chai.should;
+
+    suite('FoxNEOD', function () {
+
+        setup(function () {
+            console.log('Mocha setup');
+        });
+
+        suite('utils', function () {
+            test('addPixelSuffix', function () {
+                assert.strictEqual(utils.addPixelSuffix('12'), '12px', 'Adds the "px" suffix to a string passed in with no existing "px" in it.');
+                assert.strictEqual(utils.addPixelSuffix(12), '12px', 'Adds the "px" suffix to a number passed in.');
+                assert.notStrictEqual(utils.addPixelSuffix('30px'), '30px', 'Adds the "px" suffix to a string passed in that already has a "px" suffix.');
+            });
+
+            test('removePixelSuffix', function () {
+                assert.strictEqual(utils.removePixelSuffix('12'), '12', 'Removes the "px" suffix to a string passed in with no existing "px" in it.');
+                assert.strictEqual(utils.removePixelSuffix(12), '12', 'Removes the "px" suffix to a number passed in.');
+                assert.strictEqual(utils.removePixelSuffix('30px'), '30', 'Removes the "px" suffix to a string passed in that already has a "px" suffix.');
+            });
+        });
+
+    });
+
+    mocha.run();
+});
+
+
+
+
+
+
+//require.config({
+//    baseUrl: '/backbone-tests/',
+//    paths: {
+//        'jquery'        : '/app/libs/jquery',
+//        'underscore'    : '/app/libs/underscore',
+//        'backbone'      : '/app/libs/backbone',
+//        'mocha'         : 'libs/mocha',
+//        'chai'          : 'libs/chai',
+//        'chai-jquery'   : 'libs/chai-jquery',
+//        'models'        : '/app/models'
+//    },
+//    shim: {
+//        'underscore': {
+//            exports: '_'
+//        },
+//        'jquery': {
+//            exports: '$'
+//        },
+//        'backbone': {
+//            deps: ['underscore', 'jquery'],
+//            exports: 'Backbone'
+//        },
+//        'chai-jquery': ['jquery', 'chai']
+//    },
+//    urlArgs: 'bust=' + (new Date()).getTime()
+//});
+//
+//require(['require', 'chai', 'chai-jquery', 'mocha', 'jquery'], function(require, chai, chaiJquery){
+//
+//    // Chai
+//    var should = chai.should();
+//    chai.use(chaiJquery);
+//
+//    /*globals mocha */
+//    mocha.setup('bdd');
+//
+//    require([
+//        'specs/model-test.js',
+//    ], function(require) {
+//        mocha.run();
+//    });
+//
+//});
