2b1d2cdfd23cdd876f30e751256bb632f608650f
Brandon Aaskov <aaskov@gmail.com>
2013-08-22 17:46:23 -0700

    Commenting out config for the time being

--- js/foxneod.js
+++ js/foxneod.js
@@ -8,7 +8,6 @@ define([
     'polyfills',
     'utils',
     'player',
-    'config',
     'query',
     'system',
     'base64',
@@ -17,7 +16,7 @@ define([
     'advertising',
     'analytics',
     'storage'
-], function (_, $, Dispatcher, Debug, polyfills, utils, player, config, query, system, base64, mvpd, ovp, advertising, analytics, storage) {
+], function (_, jquery, Dispatcher, Debug, polyfills, utils, player, query, system, base64, mvpd, ovp, advertising, analytics, storage) {
     'use strict';
 
     //////////////////////////////////////////////// instance variables
@@ -65,7 +64,7 @@ define([
         //show site modal
         if (_.has(window, 'VideoAuth') && _.has(window.VideoAuth, 'Modal') && (!_.isEmpty(title) && !_.isEmpty(message)))
         {
-            var $htmlFragment = $('<div id="foxneod-error">\n    <h1>Warning</h1>\n    <p class="error-message">' + message + '</p>\n</div>');
+            var $htmlFragment = jquery('<div id="foxneod-error">\n    <h1>Warning</h1>\n    <p class="error-message">' + message + '</p>\n</div>');
 
             window.VideoAuth.Modal.open(null, title);
             window.VideoAuth.Modal.content.set($htmlFragment);
@@ -82,36 +81,44 @@ define([
         _patchIE8Problems();
         _messageUnsupportedUsers();
     };
+
+
     ////////////////////////////////////////////////
 
 
     // Public API
     return {
+        //private
         _init: init,
+
+        //properties
         buildDate: '@@buildDate',
         packageName: '@@packageName',
         version: '@@version',
-        dispatcher: dispatcher,
-        dispatch: dispatcher.dispatch,
+
+        //event listening
         on: dispatcher.on,
-        addEventListener: dispatcher.on,
+        dispatch: dispatcher.dispatch,
         getEventListeners: dispatcher.getEventListeners,
         hasEventListener: dispatcher.hasEventListener,
         removeEventListener: dispatcher.removeEventListener,
-        advertising: advertising,
+
+        //modules being surfaced
         analytics: analytics,
+        advertising: advertising,
+//        config: config,
         Debug: Debug,
+        dispatcher: dispatcher,
         mvpd: mvpd,
         ovp: ovp,
         player: player,
-        config: config,
         query: query,
+        storage: storage,
         system: system,
         utils: utils,
-        _: _,
-        jQuery: $,
 
-        //private/testing
-        _storage: storage
+        //internal libraries being used
+        _: _,
+        jQuery: jquery
     };
 });
